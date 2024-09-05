a7db5bd7874cb1704b20f40ad45dae1524030f9a
Kabir <kabir.rajkotia@gmail.com>
2024-09-05 20:51:34 +0530

    api v1

--- Label Scanner/CameraView.swift
+++ Label Scanner/CameraView.swift
@@ -2,7 +2,43 @@
 //  CameraView.swift
 //  Label Scanner
 //
-//  Created by Kabir Rajkotia on 8/16/24.
+//  Created by Kabir Rajkotia on 9/4/24.
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: CameraView
+
+        init(parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                // Pass the image to the parent for further processing
+                parent.onImagePicked(image)
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+
+    @Environment(\.presentationMode) var presentationMode
+    var onImagePicked: (UIImage) -> Void
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+}
--- Label Scanner/recognizeText.swift
+++
@@ -1,8 +0,0 @@
-//
-//  recognizeText.swift
-//  Label Scanner
-//
-//  Created by Kabir Rajkotia on 8/16/24.
-//
-
-import Foundation
